1fbaa7a64924865aeee3b3cf3c52198ca18227ab
Tahara Yoshinori <read.eval.print@gmail.com>
2024-03-23 10:09:34 +0900

    fader

--- dbw/Fader.cpp
+++ dbw/Fader.cpp
@@ -19,13 +19,20 @@ Fader::Fader(std::string name, Track* track) : BuiltinModule(name, track)
 
 bool Fader::process(ProcessBuffer* buffer, int64_t steadyTime)
 {
-    _peakValue = 0.0f;
     double peakHoldTime = gPreference.sampleRate * 3.0;
-    _peakSampleElapsed += buffer->_framesPerBuffer;
-    if (_peakSampleElapsed > peakHoldTime)
+    _peakValueLeft = 0.0f;
+    _peakValueRight = 0.0f;
+    _peakSampleElapsedLeft += buffer->_framesPerBuffer;
+    if (_peakSampleElapsedLeft > peakHoldTime)
     {
-        _peakValueHold = std::max(0.0f, _peakValueHold - 0.02f);
-        _peakSampleElapsed -= gPreference.sampleRate * 0.1;
+        _peakValueHoldLeft = std::max(0.0f, _peakValueHoldLeft - 0.02f);
+        _peakSampleElapsedLeft -= gPreference.sampleRate * 0.1;
+    }
+    _peakSampleElapsedRight += buffer->_framesPerBuffer;
+    if (_peakSampleElapsedRight > peakHoldTime)
+    {
+        _peakValueHoldRight = std::max(0.0f, _peakValueHoldRight - 0.02f);
+        _peakSampleElapsedRight -= gPreference.sampleRate * 0.1;
     }
 
     if (_mute)
@@ -40,6 +47,7 @@ bool Fader::process(ProcessBuffer* buffer, int64_t steadyTime)
     // pan の処理ってどうやるのが正しい？
     float pan = (1.0f - _pan) * 2.0f;
     float gainRatio = linearToGainRatio(_level);
+    float* peakValue = &_peakValueLeft;
     for (auto [in, out, acp, bcp] : std::views::zip(buffer->_in[0].buffer32(), buffer->_out[0].buffer32(), buffer->_in[0]._constantp, buffer->_out[0]._constantp))
     {
         auto in0 = in[0];
@@ -60,57 +68,115 @@ bool Fader::process(ProcessBuffer* buffer, int64_t steadyTime)
                 b = a * gainRatio * pan;
             }
 
-            if (_peakValue < b)
+            if ((*peakValue) < b)
             {
-                _peakValue = b;
+                (*peakValue) = b;
             }
         }
         pan = _pan * 2.0f;
+        peakValue = &_peakValueRight;
     }
 
-    if (_peakValueHold < _peakValue)
+    if (_peakValueHoldLeft < _peakValueLeft)
     {
-        _peakValueHold = _peakValue;
-        _peakSampleElapsed = 0;
+        _peakValueHoldLeft = _peakValueLeft;
+        _peakSampleElapsedLeft = 0;
+    }
+    if (_peakValueHoldRight < _peakValueRight)
+    {
+        _peakValueHoldRight = _peakValueRight;
+        _peakSampleElapsedRight = 0;
     }
 
     return Module::process(buffer, steadyTime);
 }
 
-void Fader::renderContent()
+void Fader::render(float width, float height)
 {
+    ImGui::PushID(this);
+    ImVec2 windowPos = ImGui::GetWindowPos();
+    ImGuiChildFlags childFlags = ImGuiChildFlags_Border;
+    ImGuiWindowFlags windowFlags = ImGuiWindowFlags_None;
+    if (ImGui::BeginChild("##fader", ImVec2(width, height), childFlags, windowFlags))
+    {
+        ImGui::BeginGroup();
 
-    ImGui::BeginGroup();
-    ImVec2 pos1 = ImGui::GetCursorPos() + ImGui::GetWindowPos();
-    ImVec2 pos2 = pos1 + ImVec2(10.0f, 100.0f);
-    ImDrawList* drawList = ImGui::GetWindowDrawList();
-    drawList->AddRectFilled(pos1, pos2, IM_COL32(0x80, 0xff, 0x80, 0x80));
-    pos1.y += (pos2.y - pos1.y) * (1.0f - std::min(1.0f, _peakValue));
-    drawList->AddRectFilled(pos1, pos2, IM_COL32(0x00, 0x80, 0xff, 0x80));
-    pos1 = ImGui::GetCursorPos() + ImGui::GetWindowPos();
-    pos1.y += (pos2.y - pos1.y) * (1.0f - std::min(1.0f, _peakValueHold));
-    pos2 = pos1 + ImVec2(10.0f, 2.0f);
-    drawList->AddRectFilled(pos1, pos2, IM_COL32(0xff, 0, 0, 0x80));
-    ImGui::VSliderFloat("VU", ImVec2(5.0f, 100.0f), &_level, 0.0f, 1.0f);
-    ImGui::EndGroup();
+        float windowHeight = ImGui::GetWindowHeight();
+        auto& style = ImGui::GetStyle();
+        float meterHeight = windowHeight - style.WindowPadding.y * 2;
+        float meterWidth = 20.0f;
 
-    ImGui::SameLine();
+        ImVec2 meterPos1 = ImGui::GetCursorPos() + ImGui::GetWindowPos();
+        ImVec2 meterPos2 = meterPos1 + ImVec2(meterWidth, meterHeight);
+        ImDrawList* drawList = ImGui::GetWindowDrawList();
+        drawList->AddRect(meterPos1, meterPos2, gTheme.rackBorder);
 
-    ImGui::BeginGroup();
-    ImGui::PushItemWidth(-FLT_MIN);
-    ImGui::SliderFloat("##Level", &_level, 0.0f, 1.0f, "Level %.3f");
-    ImGui::SliderFloat("##Pan", &_pan, 0.0f, 1.0f, "Pan %.3f");
-    ImGui::Checkbox("Mute", &_mute);
-    ImGui::SameLine();
-    ImGui::Checkbox("Solo", &_solo);
-    ImGui::Text(std::to_string(_computedLatency).c_str());
-    ImGui::PopItemWidth();
-    ImGui::EndGroup();
 
-    float gr = linearToGainRatio(_level);
-    float dB = gainRatioToDB(gr);
-    ImGui::Text(std::format("{}dB", dB).c_str());
-    ImGui::Text(std::format("{}gr", gr).c_str());
+        ImVec2 pos1 = meterPos1 + ImVec2(0.0f, meterHeight * (1.0f - ZERO_DB_NORMALIZED_VALUE));
+        ImVec2 pos2 = pos1 + ImVec2(meterWidth, 0.0f);
+        drawList->AddLine(pos1, pos2, gTheme.rackBorder);
+        drawList->AddText(pos1 + ImVec2(7.0f, 0.0f), gTheme.text, "0");
+        
+        pos1 = meterPos1 + ImVec2(0.0f, meterHeight * (1.0f - dbToLinear(-6.0f)));
+        pos2 = pos1 + ImVec2(meterWidth, 0.0f);
+        drawList->AddLine(pos1, pos2, gTheme.rackBorder);
+        drawList->AddText(pos1 + ImVec2(7.0f, 0.0f), gTheme.text, "6");
+
+        pos1 = meterPos1 + ImVec2(0.0f, meterHeight * (1.0f - dbToLinear(-12.0f)));
+        pos2 = pos1 + ImVec2(meterWidth, 0.0f);
+        drawList->AddLine(pos1, pos2, gTheme.rackBorder);
+        drawList->AddText(pos1 + ImVec2(7.0f, 0.0f), gTheme.text, "12");
+
+
+        pos1 = meterPos1;
+        pos2 = meterPos2 - ImVec2(10.0f, 0);
+        pos1.y += (pos2.y - pos1.y) * (1.0f - std::min(1.0f, _peakValueLeft));
+        drawList->AddRectFilled(pos1, pos2, IM_COL32(0x00, 0x80, 0xff, 0x80));
+
+        pos1 = meterPos1 + ImVec2(10.0f, 0);
+        pos2 = meterPos2;
+        pos1.y += (pos2.y - pos1.y) * (1.0f - std::min(1.0f, _peakValueRight));
+        drawList->AddRectFilled(pos1, pos2, IM_COL32(0x00, 0x80, 0xff, 0x80));
+
+        if (_peakValueHoldLeft > 0.001f)
+        {
+            pos1 = meterPos1;
+            pos1.y += meterHeight * (1.0f - std::min(1.0f, _peakValueHoldLeft)) - 1.0f;
+            pos2 = pos1 + ImVec2(10.0f, 1.0f);
+            drawList->AddRectFilled(pos1, pos2, IM_COL32(0xff, 0, 0, 0x80));
+        }
+        if (_peakValueHoldRight > 0.001f)
+        {
+            pos1 = meterPos1 + ImVec2(10.0f, meterHeight * (1.0f - std::min(1.0f, _peakValueHoldRight)) - 1.0f);
+            pos2 = pos1 + ImVec2(10.0f, 1.0f);
+            drawList->AddRectFilled(pos1, pos2, IM_COL32(0xff, 0, 0, 0x80));
+        }
+
+        {
+            float gainRatio = linearToGainRatio(_level);
+            float db = normalizedValueToDb(gainRatio);
+            ImGui::VSliderFloat("##level", ImVec2(20.0f, meterHeight), &_level, 0.0f, 1.0f, std::format("{:.3}", db).c_str());
+        }
+
+        ImGui::EndGroup();
+
+        ImGui::SameLine();
+
+        ImGui::BeginGroup();
+        ImGui::Text(std::format("{:> 4.4}dB {:> 4.4}dB",
+                                normalizedValueToDb(_peakValueHoldLeft),
+                                normalizedValueToDb(_peakValueHoldRight)).c_str());
+        ImGui::PushItemWidth(-FLT_MIN);
+        ImGui::SliderFloat("##Pan", &_pan, 0.0f, 1.0f, "Pan %.3f");
+        ImGui::Checkbox("Mute", &_mute);
+        ImGui::SameLine();
+        ImGui::Checkbox("Solo", &_solo);
+        ImGui::Text(std::to_string(_computedLatency).c_str());
+        ImGui::PopItemWidth();
+        ImGui::EndGroup();
+    }
+    ImGui::EndChild();
+    ImGui::PopID();
 }
 
 nlohmann::json Fader::toJson(SerializeContext& context)
@@ -120,17 +186,36 @@ nlohmann::json Fader::toJson(SerializeContext& context)
     return json;
 }
 
-float Fader::gainRatioToDB(float gainRatio)
+float Fader::gainRatioToLinear(float gainRatio)
 {
-    if (gainRatio == 0.0f) return -180.0f;
-    float dB = 20.0f * log10(gainRatio);
-    return dB;
+    float a = 1.0f / (ZERO_DB_NORMALIZED_VALUE * ZERO_DB_NORMALIZED_VALUE * ZERO_DB_NORMALIZED_VALUE);
+    float linear = cbrt((gainRatio - 0.00000006f) / a);
+    return linear;
 }
+
+float Fader::dbToNormalizedValue(float db)
+{
+    if (db <= -180.0f) return 0.0f;
+    float gainRatio = pow(10, db / 20.0f);
+    return gainRatio;
+}
+
+float Fader::dbToLinear(float db)
+{
+    return gainRatioToLinear(dbToNormalizedValue(db));
+}
+
 
 float Fader::linearToGainRatio(float linear)
 {
-    float a = 1.0f / (0.7f * 0.7f * 0.7f);
+    float a = 1.0f / (ZERO_DB_NORMALIZED_VALUE * ZERO_DB_NORMALIZED_VALUE * ZERO_DB_NORMALIZED_VALUE);
     float gainRatio = a * linear * linear * linear + 0.00000006f;
     return gainRatio;
 }
 
+float Fader::normalizedValueToDb(float value)
+{
+    if (value == 0.0f) return -180.0f;
+    float dB = 20.0f * log10(value);
+    return dB;
+}
--- dbw/Fader.h
+++ dbw/Fader.h
@@ -3,15 +3,17 @@
 
 class Fader : public BuiltinModule {
 public:
+    const float ZERO_DB_NORMALIZED_VALUE = 0.7f;
+
     Fader(const nlohmann::json& json, SerializeContext& context);
     Fader(std::string name, Track* track);
     virtual ~Fader() = default;
     bool process(ProcessBuffer* buffer, int64_t steadyTime) override;
-    void renderContent() override;
+    void render(float width = 0.0f, float height = 0.0f) override;
 
     virtual nlohmann::json toJson(SerializeContext& context) override;
 
-    float _level = 1.0f;
+    float _level = ZERO_DB_NORMALIZED_VALUE;
     float _pan = 0.5f;
     bool _mute = false;
     bool _solo = false;
@@ -19,11 +21,17 @@ public:
     NLOHMANN_DEFINE_TYPE_INTRUSIVE(Fader, _level, _pan, _mute, _solo);
 
 private:
+    float dbToNormalizedValue(float db);
+    float dbToLinear(float db);
     float linearToGainRatio(float linearValue);
-    float gainRatioToDB(float gainRatio);
+    float gainRatioToLinear(float gainRatio);
+    float normalizedValueToDb(float linear);
 
-    float _peakValue = 0.0f;
-    float _peakValueHold = 0.0f;
-    int _peakSampleElapsed = 0;
+    float _peakValueLeft = 0.0f;
+    float _peakValueRight = 0.0f;
+    float _peakValueHoldLeft = 0.0f;
+    float _peakValueHoldRight = 0.0f;
+    int _peakSampleElapsedLeft = 0;
+    int _peakSampleElapsedRight = 0;
 };
 
